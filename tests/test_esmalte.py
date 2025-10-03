ebb999bfa0d02b293f6ad0d54a1e4fc0c52fa89f
Luzía <luciaglsn@correo.ugr.es>
2025-10-03 15:00:00 +0200

    Corrige según el lint y el autoformatter

--- tests/test_esmalte.py
+++ tests/test_esmalte.py
@@ -1,7 +1,8 @@
 import pytest
-from proyecto_tfg.esmalte import Esmalte
-from proyecto_tfg.campo import Campo
-from proyecto_tfg.escudo import Escudo
+from proyectotfg.esmalte import Esmalte
+from proyectotfg.campo import Campo
+from proyectotfg.escudo import Escudo
+
 
 @pytest.mark.parametrize("nombre", ["marron", "", "  ", "fucsia"])
 def test_esmalte_invalido_impide_crear_escudo(nombre):
