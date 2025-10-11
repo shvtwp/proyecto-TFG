042a2fb629ef122c741bc45f932fccf2aff0a0ba
Luzía <luciaglsn@correo.ugr.es>
2025-10-11 15:19:57 +0200

    Pone en verde test mueble #78

--- heraldica/campo.py
+++ heraldica/campo.py
@@ -1,11 +1,17 @@
 from dataclasses import dataclass
 from .esmalte import Esmalte
-
+from .mueble import Mueble
+from typing import List
 
 @dataclass(frozen=True)
 class Campo:
     esmalte: Esmalte
+    muebles: List[Mueble]
+
 
     def __post_init__(self):
         if not isinstance(self.esmalte, Esmalte):
             raise TypeError("Campo requiere un Esmalte válido.")
+        for m in self.muebles:
+            if not isinstance(m, Mueble):
+                raise TypeError("Todos los muebles deben ser instancias de Mueble")
